72b8ecdf83d609b3a5b97664631bcff1404777b3
Jman45-6 <jeremy.cope7142@gmail.com>
2023-06-29 15:23:24 -0700

    added named params

--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
+    //* getting named params
+    char operation[50];
+    char binName[50];
+    char newBinPath[200];
+    for (int i = 1; i < argc; i += 2)
+    {
+        if (i + 1 >= argc)
+        {
+            printf("Please Enter a value for: %s\n", argv[i]);
+            continue;
+        }
 
+        if (strcmp(argv[i], "--operation") == 0)
+        {
+            strcpy(operation, argv[i + 1]);
+        }
+        else if (strcmp(argv[i], "--binName") == 0)
+        {
+            strcpy(binName, argv[i + 1]);
+        }
+        else if (strcmp(argv[i], "--newBinPath") == 0)
+        {
+            strcpy(newBinPath, argv[i + 1]);
+        }
+        else
+        {
+            printf("%s is not a valid parameter\n", argv[i]);
+        }
+    }
+
+    printf("Operation: %s\n", operation);
+    printf("Bin Name: %s\n", binName);
+    printf("newBinPath: %s\n", newBinPath);
     return 0;
 }
