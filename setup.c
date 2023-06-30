0482fb35f74157266a80afbf6b3fea1c41dd1a52
Jman45-6 <jeremy.cope7142@gmail.com>
2023-06-29 20:25:06 -0700

    added appeneding bin path to config file

--- setup.c
+++ setup.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 
-int binSetup()
+int binSetup(FILE *configFile)
 {
     printf("Where would you like your bins?: ");
     char binPath[500];
     scanf("%s", binPath);
+    fprintf(configFile, "binPath: %s\n", binPath);
 }
 
 int main(int argc, char *argv[])
 {
-    binSetup();
+    FILE *configFile = fopen("config.yaml", "w");
+    binSetup(configFile);
+    fclose(configFile);
     return 0;
 }
